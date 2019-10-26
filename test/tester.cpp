24a13b0e65cdb766f0d874f8c2013113e0e4973a
RICARDO MOREIRA <160143756@aluno.unb.br>
2019-10-26 20:16:00 -0300

    Criação do caso de teste de Codificação de Manchester

--- test/tester.cpp
+++ test/tester.cpp
@@ -1,2 +1,28 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
+
+#include <vector>
+#include <bitset>
+
+#include "CamadaFisica.h"
+
+TEST_CASE("O quadro 01001110 deve retornar 01100101 10101001 na codificacao de \
+    manchester", "[codificacao]") {
+  std::vector<std::bitset<8>> quadros;
+  std::vector<std::bitset<8>> manchester_quadros;
+  std::vector<std::bitset<8>> manchester_quadros_esperado;
+
+  std::bitset<8> mensagem;
+  mensagem = 0b01001110;
+  quadros.push_back(mensagem);
+
+  manchester_quadros = CamadaFisicaTransmissoraCodificacaoManchester(quadros);
+
+  manchester_quadros_esperado.push_back(std::bitset<8> (0b10101001));
+  manchester_quadros_esperado.push_back(std::bitset<8> (0b01100101));
+
+  REQUIRE(manchester_quadros.size() == 2);
+  REQUIRE(manchester_quadros[0] == manchester_quadros_esperado[0]);
+  REQUIRE(manchester_quadros[1] == manchester_quadros_esperado[1]);
+}
+
